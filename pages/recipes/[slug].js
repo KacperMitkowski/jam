7ab2c19f28699206c6574746c92009f302463156
Kacper Mitkowski <kacper.mitkowski@medicover.pl>
2021-08-18 17:07:56 +0200

    redirect

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -25,6 +25,16 @@ export const getStaticPaths = async () => {
 
 export async function getStaticProps({ params }) {
   const { items } = await client.getEntries({ content_type: 'recipe', 'fields.slug': params.slug });
+  
+  if(!items.length) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
       recipe: items[0],
